6d70c29df4b12cb2d3a0223790c4145dfc07f45f
Anastasiia <an.digich@gmail.com>
2020-03-03 21:03:10 +0200

    1

--- l1/script.js
+++ l1/script.js
@@ -1,4 +1,9 @@
-var a=2;
-console.log(a);
-var srt ="Hello";
-console.log(str);
+ var arr =[-1, 0,1,2,3];
+
+arr.forEach(function(num) {
+    console.log(num);
+});
+var Arr = arr.map(function(num){return num**2;});
+console.log(Arr);
+var Arr2 =arr.filter(function(num){return num>1;});
+console.log(Arr2);
